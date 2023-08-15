76c703231600c9095a87996041d3d0d7c4c5ea81
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-15 15:20:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const li = document.querySelectorAll("li");
+const level = document.getElementById('level')
+const ind = Array.from(li).indexOf(level);
+
+alert(`The level of the element is: ${ind}`);
