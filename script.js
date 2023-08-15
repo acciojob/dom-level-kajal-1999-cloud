693efbd1332dd1c13cda9d527a245d004a7e0932
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-15 15:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 const li = document.querySelectorAll("li");
+
 const level = document.getElementById('level')
+// Convert the NodeList to an array and find the index of the element
 const ind = Array.from(li).indexOf(level);
 
 alert(`The level of the element is: ${ind}`);
